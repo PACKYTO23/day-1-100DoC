b7b8d4e37d49fc8d2f391c436255167b08942785
Francisco_GP <91037607+PACKYTO23@users.noreply.github.com>
2023-11-29 15:52:52 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,71 @@
+# print("Hello, World!")
+
+# / # / # EXERCISE # / # / #
+
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
+
+# / # / # / # / # / #
+
+# print("Hello, World!\nHello, World!\nHello, World!")
+
+# print("Hello, " + "Francisco")
+
+# / # / # EXERCISE # / # / #
+
+# print(Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+#     print('e.g. print("Hello " + "world")')
+# print(("New lines can be created with a backslash and n.")
+
+# print("Day 1 - String Manipulation")
+# print('String Concatenation is done with the "+" sign.')
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+# / # / # / # / # / #
+
+# print("Hello, " + input("What is your name? ") + "!")
+
+# / # / # EXERCISE # / # / #
+
+# print(len(input("What is your name? ")))
+
+# / # / # / # / # / #
+
+# name = input("What is your name? ")
+# print(name)
+
+# name = "Juan"
+# print(name)
+
+# name = "Mariano"
+# print(name)
+
+# name = input("What is your name? ")
+# length = len(name)
+# print(length)
+
+# a = input("Input of a: ")
+# b = input("Input of b: ")
+
+# new_a = a
+# new_b = b
+# a = new_b
+# b = new_a
+
+# # # Alternative answer:
+# # c = a
+# # a = b
+# # b = c
+
+# print("a: " + a)
+# print("b: " + b)
+
+# / # / # PROJECT OF DAY 1 # / # / #
+
+print("Welcome to the Band Name Generator.")
+name = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + name + " " + pet)
